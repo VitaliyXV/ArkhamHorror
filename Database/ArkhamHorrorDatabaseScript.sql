e139fbc183bbd64b5afcea4d618b9cf95c7ec7c5
VitaliyKharchenko <vitaliy.harchenko@ilogos-ua.com>
2016-08-23 17:52:59 +0300

    Create script for MS SQL

--- Database/ArkhamHorrorDatabaseScript.sql
+++ Database/ArkhamHorrorDatabaseScript.sql
@@ -9,6 +9,18 @@ create table game_extentions
 	release_year int
 ) character set=utf8;
 
+insert into game_extentions (original_name, local_name, description, release_year)
+					values	('Arkham Horror', 'Ужас Аркхема', 'Базовая игра', 2005);
+
+create table colors
+(
+	id int primary key auto_increment,
+	original_name nvarchar(50) not null,
+	local_name nvarchar(50) not null
+) character set=utf8;
+
+insert into colors (original_name, local_name) values ('Black', 'Черный');
+
 create table monster_move_types
 (
 	id int primary key auto_increment,
@@ -17,12 +29,14 @@ create table monster_move_types
 	description nvarchar(1000)
 ) character set=utf8;
 
-create table colors
-(
-	id int primary key auto_increment,
-	original_name nvarchar(50) not null,
-	local_name nvarchar(50) not null
-) character set=utf8;
+alter table monster_move_types
+	add color int;
+
+alter table monster_move_types	
+	add foreign key (color) references colors(id);
+
+insert into monster_move_types 	(original_name, local_name, description, color)
+						values	('Normal', 'Обычный', 'Монстр движется по стандартным правилам.', 1);
 
 create table dimensions
 (
@@ -34,6 +48,9 @@ create table dimensions
 	foreign key (game_extention) references game_extentions(id)
 ) character set=utf8;
 
+insert into dimensions 	(original_name, local_name, description, game_extention)
+						values	('The Abyss', 'Бездна', '', 1);
+
 create table dimensions_colors
 (
 	dimension int,
@@ -41,6 +58,8 @@ create table dimensions_colors
 	foreign key (color) references colors(id),
 	foreign key (dimension) references dimensions(id)
 ) character set=utf8;
+
+insert into dimensions_colors (dimension, color) values(1, 11);
 
 create table monster_types
 (
@@ -50,14 +69,18 @@ create table monster_types
 	description nvarchar(1000)
 ) character set=utf8;
 
+insert into monster_types (original_name, local_name, description) values ('Simple', 'Обычный', '');
+
 create table abilities
 (
 	id int primary key auto_increment,
 	original_name nvarchar(50) not null,
 	local_name nvarchar(50) not null,
 	description nvarchar(1000),
-	volume int
+	volume int default 0
 ) character set=utf8;
+
+insert into abilities (original_name, local_name, description, volume) values ('Ambush', 'Засада', 'Из боя с таким монстром отступить нельзя, сыщик должен драться до победы или поражения.', 0);
 
 create table monsters
 (
@@ -81,14 +104,23 @@ create table monsters
 	foreign key (monster_type) references monster_types(id)
 ) character set=utf8;
 
+insert into monsters (original_name, local_name, description, game_extention, monster_move_type, monster_type,
+						toughness, dimension, awareness, horror_rating, horror_damage, combat_rating, combat_damage) 
+			values	('Formless Spawn', 'Бесформенная тварь', 'Из мрака глухого переулка вырвалось пятно тени. Оно взвилось по кирпичной стене и заступило нам путь.',
+						1, 11, 1, 2, 1, 0, -1, 2, -2, 2);
+
+--select * from monsters;
+
 create table monsters_count
 (
 	monster int,
-	dimension int,	
+	game_extention int,	
 	count int,
 	foreign key (monster) references monsters(id),
-	foreign key (dimension) references dimensions(id)
+	foreign key (game_extention) references game_extentions(id)
 ) character set=utf8;
+
+insert into monsters_count (monster, game_extention, count) values (1, 1, 2);
 
 create table monsters_abilities
 (
@@ -97,3 +129,5 @@ create table monsters_abilities
 	foreign key (monster) references monsters(id),
 	foreign key (ability) references abilities(id)
 ) character set=utf8;
+
+insert into monsters_abilities (monster, ability) values (1, 1);
---
+++ Database/ArkhamHorrorMSSQLDatabaseScript.sql
@@ -0,0 +1,136 @@
+﻿use arkham_horror;
+
+-----------------------------
+
+--drop table MonstersAbilities;
+--drop table MonstersAmount;
+--drop table Monsters;
+--drop table Abilities;
+--drop table MonsterTypes;
+--drop table DimensionsColors;
+--drop table Dimensions;
+--drop table MonsterMoveTypes;
+--drop table Colors;
+--drop table GameExtentions;
+
+----------------------------
+
+create table GameExtentions
+(
+	Id int primary key identity,
+	OriginalName nvarchar(50) not null,
+	LocalName nvarchar(50) not null,
+	[Description] nvarchar(1000) default '',
+	ReleaseYear int not null
+);
+
+create table Colors
+(
+	Id int primary key identity,
+	OriginalName nvarchar(50) not null,
+	LocalName nvarchar(50) not null
+);
+
+create table MonsterMoveTypes
+(
+	Id int primary key identity,
+	OriginalName nvarchar(50) not null,
+	LocalName nvarchar(50) not null,
+	Color int foreign key references Colors(id) not null,
+	[Description] nvarchar(1000) default ''
+);
+
+create table Dimensions
+(
+	Id int primary key identity,
+	OriginalName nvarchar(50) not null,
+	LocalName nvarchar(50) not null,
+	[Description] nvarchar(1000) default '',
+	GameExtention int foreign key references GameExtentions(id) not null
+);
+
+create table DimensionsColors
+(
+	Dimension int foreign key references Dimensions(id) not null,
+	Color int foreign key references Colors(id) not null
+);
+
+create table MonsterTypes
+(
+	Id int primary key identity,
+	OriginalName nvarchar(50) not null,
+	LocalName nvarchar(50) not null,
+	[Description] nvarchar(1000) default ''
+);
+
+create table Abilities
+(
+	Id int primary key identity,
+	OriginalName nvarchar(50) not null,
+	LocalName nvarchar(50) not null,
+	[Description] nvarchar(1000) default '',
+	Value int default 0 not null
+);
+
+create table Monsters
+(
+	Id int primary key identity,
+	OriginalName nvarchar(50) not null,
+	LocalName nvarchar(50) not null,
+	[Description] nvarchar(1000) default '',
+	GameExtention int foreign key references GameExtentions(id) not null,
+	MonsterMoveType int foreign key references MonsterMoveTypes(id) not null,
+	MonsterType int foreign key  references MonsterTypes(id) not null,	
+	Dimension int foreign key references Dimensions(id) not null,-- мир
+	Toughness int default 0 not null,		-- стойкость
+	Awareness int default 0 not null, 		-- бдительность
+	HorrorRating int default 0 not null,	-- ужас
+	HorrorDamage int default 0 not null,	-- урон ужаса
+	CombatRating int default 0 not null,	-- боевой рейтинг
+	CombatDamage int default 0 not null		-- боевой урон	
+);
+
+create table MonstersAmount
+(
+	Monster int foreign key references monsters(id) not null,
+	GameExtention int foreign key references GameExtentions(id) not null,	
+	Amount int default 0 not null
+);
+
+create table MonstersAbilities
+(
+	Monster int foreign key references Monsters(id) not null,
+	Ability int foreign key references Abilities(id) not null
+);
+
+---------------------------------------
+
+insert into GameExtentions (OriginalName, LocalName, [Description], ReleaseYear)
+					values	(N'Arkham Horror', N'Ужас Аркхема', N'Базовая игра', 2005);
+					
+insert into colors (OriginalName, LocalName) values (N'Black', N'Черный');
+
+insert into MonsterMoveTypes 	(OriginalName, LocalName, [Description], Color)
+						values	(N'Normal', N'Обычный', N'Монстр движется по стандартным правилам.', 1);
+						
+insert into Dimensions 	(OriginalName, LocalName, [Description], GameExtention)
+						values	(N'The Abyss', N'Бездна', '', 1);
+						
+insert into DimensionsColors (Dimension, Color) values(1, 1);
+
+insert into MonsterTypes (OriginalName, LocalName, [Description]) values (N'Simple', N'Обычный', '');
+
+insert into Abilities (OriginalName, LocalName, [Description], Value) values (N'Ambush', N'Засада', N'Из боя с таким монстром отступить нельзя, сыщик должен драться до победы или поражения.', 0);
+
+insert into Monsters (OriginalName, LocalName, [Description], GameExtention, MonsterMoveType, MonsterType,
+						Toughness, Dimension, Awareness, HorrorRating, HorrorDamage, CombatRating, CombatDamage) 
+			values	(N'Formless Spawn', N'Бесформенная тварь', N'Из мрака глухого переулка вырвалось пятно тени. Оно взвилось по кирпичной стене и заступило нам путь.',
+						1, 1, 1, 2, 1, 0, -1, 2, -2, 2);
+
+insert into MonstersAmount (Monster, GameExtention, Amount) values (1, 1, 2);
+
+insert into MonstersAbilities (Monster, Ability) values (1, 1);
+
+---------------------------------------
+
+select * from monsters;
