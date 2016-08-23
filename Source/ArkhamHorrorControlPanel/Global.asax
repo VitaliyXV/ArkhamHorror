3631f6b1e1c2a60d26e9b6f87cc0e18aac83169c
VitaliyKharchenko <vitaliy.harchenko@ilogos-ua.com>
2016-08-23 07:53:24 +0300

    Create web application

---
+++ Source/ArkhamHorrorControlPanel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArkhamHorrorControlPanel.MvcApplication" Language="C#" %>
---
+++ Source/ArkhamHorrorControlPanel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArkhamHorrorControlPanel.Startup))]
+namespace ArkhamHorrorControlPanel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
