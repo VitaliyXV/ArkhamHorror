0912ebcf699c9d253faf04b5e88f8c1152d97ea5
STEP\xv <vitaliyxv@gmail.com>
2016-09-01 21:02:56 +0300

    Add main menu

---
+++ Source/ArkhamHorrorTool/ArkhamHorrorTool/Assets/Scripts/MenuItemScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MenuItemScript : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("MenuItemStart");
+    }
+
+    public void OnMenuItemClick()
+    {
+        Debug.Log("Menu Item Click!");
+    }
+}
